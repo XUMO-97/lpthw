c2344159b42070d84506a0ae43912d8a768a79fb
XUMO-97 <184026603@qq.com>
2018-09-02 23:07:48 +0800

    the first exercise

---
+++ ex1.py
@@ -0,0 +1,31 @@
+# exercise 1
+print "Hello World!" # print statement makes powershell display what is in double quotes.
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.' # single quotes is avalible.
+print "I'd much rather you 'not'." # single quotes in double quotes work as well.
+print 'I "said" do not touch this.'  # double in single too.
+
+# study drills 1:Make your script print another line.
+print "Hello World!" 
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.' 
+print "I'd much rather you 'not'." 
+print 'I "said" do not touch this.' 
+print "another line."
+
+# study drills 2:Make your script print only one of the lines.
+print "Hello World!" # writer is right,skip it and come back later,finally know why he said this repeatedly.
+# print "Hello Again"
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! Printing.' 
+# print "I'd much rather you 'not'." 
+# print 'I "said" do not touch this.' 
+
+# study drills 3:Put a "#" (octothorpe) haracter at the beginning of a line. What did it do? Try to find out what this character does.
+# print "There is nothing."
+# "#"character means comment out.
