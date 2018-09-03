02f3137b7f364ce23418fbf856c09fc737ee2ffb
XUMO-97 <184026603@qq.com>
2018-09-03 22:57:22 +0800

    add exercise 2 file

---
+++ ex2/ex2.py
@@ -0,0 +1,23 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print "This will run."
+
+# Study Drills 2
+
+# 1.Find out if you were right about what the # character does and make sure you know what it’s called (octothorpe or pound character)
+# I will call it pound or jinghao.
+
+# 2.Take your ex2.py fi le and review each line going backward. Start at the last line, and check each word in reverse against what you should have typed.
+# It's all done.
+
+# 3.Did you fi nd more mistakes? Fix them.
+# No.
+
+# 4.Read what you typed previously out loud, including saying each character by its name. Did you fi nd more mistakes? Fix them.
+# Too much nonsense XD.
