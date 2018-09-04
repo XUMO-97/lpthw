601add43fef57255d7064175f2aa3ef6d9ff582b
XUMO-97 <184026603@qq.com>
2018-09-04 22:56:27 +0800

    add exercise 3 file

---
+++ ex3/ex3.py
@@ -0,0 +1,95 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# study drills 1
+
+# just print some words
+print "I will now count my chickens:" 
+
+# print the word "Hens" then print the calculation after it
+print "Hens", 25 + 30 / 6 
+# print the word "Roosters" then print the calculation after it
+print "Roosters", 100 - 25 * 3 % 4 
+
+# print the sentence
+print "Now I will count the eggs:" 
+
+# print the calculation
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 
+
+# print the sentence
+print "Is it true that 3 + 2 < 5 - 7?" 
+
+# print the calculation
+print 3 + 2 < 5 - 7
+
+# print sentence and calculation
+print "What is 3 + 2?", 3 + 2
+# the same as above
+print "What is 5 - 7?", 5 - 7
+
+# print sentence
+print "Oh, that's why it's False."
+
+# sentence
+print "How about some more."
+
+# sentence and calculation and the following is same
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+# study drills 3
+
+# 1 / 3 = 0 In order to cooperate with the author I feel very suprised although I know the reason
+
+# study drills 4
+
+# 1 / 3 has floating point so I should use floating-point calculation then can get the number after decimal point
+
+# study drills 5
+
+print "I will now count my chickens:"
+
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+print "Now I will count the eggs:"
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+print 3.0 + 2.0 < 5.0 - 7.0
+
+print "What is 3.0 + 2.0?", 3.0 + 2.0
+print "What is 5.0 - 7.0?", 5.0 - 7.0
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
