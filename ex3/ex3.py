02bd148e8728e22ce3aa0b7577b9adf9b655a3de
XUMO-97 <184026603@qq.com>
2018-09-07 21:44:30 +0800

    add exercise 3 file

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -22,7 +22,7 @@ print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
 
-# study drills 1
+# study drills 1:Above each line, use the # to write a comment to yourself explaining what the line does.
 
 # just print some words
 print "I will now count my chickens:" 
@@ -60,15 +60,15 @@ print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
 
-# study drills 3
+# study drills 3:Find something you need to calculate and write a new .py fi le that does it.
 
 # 1 / 3 = 0 In order to cooperate with the author I feel very suprised although I know the reason
 
-# study drills 4
+# study drills 4:Notice the math seems “wrong”? There are no fractions, only whole numbers. Find out why by researching what a “fl oating point” number is.
 
 # 1 / 3 has floating point so I should use floating-point calculation then can get the number after decimal point
 
-# study drills 5
+# study drills 5:Rewrite ex3.py to use fl oating point numbers so it’s more accurate (hint: 20.0 is fl oating point).
 
 print "I will now count my chickens:"
 
