c129c926b64a3f859994a30083798bd3cd43e8a0
XUMO-97 <184026603@qq.com>
2018-09-07 22:50:36 +0800

    add exercise 9 file

---
+++ ex9/ex9.py
@@ -0,0 +1,15 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+with the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+# study drills:Do your checks of your work, write down your mistakes, and try not to make them on the next exercise.
+# no mistake
